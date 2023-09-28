b839e061a901a9d1e53c8973fd16349f8cc50782
timtonix <timtonic@outlook.fr>
2023-09-28 09:37:46 +0200

    convertisseur

--- convertisseurs/complementadeux.py
+++ convertisseurs/complementadeux.py
@@ -4,11 +4,23 @@ import click
 @click.command()
 @click.option("--input-base", prompt="Input base", type=int)
 @click.option("--output-base", prompt="Output base", type=int)
-@click.option("--value", prompt="Value to convert", type=int)
+@click.option("--value", prompt="Value to convert", type=str)
 def get_default_params(input_base, output_base, value):
-    click.echo(input_base)
-    click.echo(output_base)
-    click.echo(value)
+    if input_base == 2 and output_base == 10:
+        binary_to_decimal(value)
+
+
+def binary_to_decimal(value: str):
+    binary_list = [*value]
+    binary_list = binary_list[::-1]
+
+    base_ten = 0
+    for i, number in enumerate(binary_list):
+        number = int(number)
+        if number == 1:
+            base_ten += 2**i
+
+    print(base_ten)
 
 
 if __name__ == '__main__':
