5d18fc022cec3c4a82190eb7910809d454e8d76a
timtonix <timtonic@outlook.fr>
2023-11-16 09:56:58 +0100

    cli coloriser

--- filtre_photo/cli.py
+++ filtre_photo/cli.py
@@ -7,7 +7,7 @@ questions = [
         "name": "filtre",
         "type": "list",
         "message": "Choisissez un filtre à appliquer",
-        "choices": ["Rouge", "Vert", "Bleu", "Gris", "Noir et Blanc", "Miroir", "Pixel", "Lumiere"]
+        "choices": ["Rouge", "Vert", "Bleu", "Gris", "Noir et Blanc", "Miroir", "Pixel", "Lumiere", "Coloriser"]
 
     }]
 
@@ -22,5 +22,12 @@ if couleur == "Miroir":
 if couleur == "Pixel":
     pixel = inquirer.number("De combien voulez-vous pixeliser l'image ?").execute()
     filtre.pixeln(int(pixel))
+if couleur == "Lumiere":
+    lum = inquirer.number("De combien voulez-vous augmenter la luminosité").execute()
+    filtre.filtre_lum(int(lum))
+if couleur == "Coloriser":
+    rgb = inquirer.rawlist("Quelle couleur voulez vous augmenter ?", ["rouge", "vert", "bleu"]).execute()
+    k = inquirer.number("De combien voulez-vous augmenter la couleur ?").execute()
+    filtre.inc_color(rgb, int(k))
 else:
     filtre.filtre_couleur(couleur)
